078e7b5deaf9a4660da7b31f042cdf38d0c5d2e4
Paulo Muniz <paulotadeums@gmail.com>
2021-06-19 17:31:33 -0300

    feat: innitial commit

---
+++ src/test/java/br/com/devforum/DevforumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
